4f79ef0872df5ca81d57ffd51586cd98677b066b
Тажиев Алмас <almas200224@gmail.com>
2024-06-01 21:41:01 +0600

    dep

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,7 @@
 import { defineConfig } from "vite";
-import React from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [
-    React({
-      rollupPluginVueOptions: {
-        template: {
-          compilerOptions: {
-            // Добавляем игнорирование предупреждений о недопустимых свойствах DOM
-            allowedElements: [
-              "svg",
-              "path",
-              "g",
-              "circle",
-              "line",
-              "rect",
-              "text",
-              "linearGradient",
-              "stop",
-              "fillRule",
-            ],
-          },
-        },
-      },
-    }),
-  ],
+  plugins: [react()],
+  base: "/Ege_store/",
 });
